378f4deb012bdeae6775d2d6da70156e772d918d
Leonardo Nascimento <leonardo@14bisdigital.com.br>
2017-01-19 15:50:00 -0200

    Preparando o frontend

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var elixir = require('laravel-elixir'),
+	bowerDir = "vendor/bower_components/";
+
+	elixir(function(mix) {
+		mix.copy(bowerDir + 'bootstrap/fonts', 'public/fonts') //Copia da pasta bootstrap/fonts para a pasta public fonts
+			.copy(bowerDir + 'fontawesome/fonts', 'public/fonts')//Copia da pasta awesome/fonts para a pasta public fonts
+	
+			.copy(bowerDir + 'bootstrap/dist/js/bootstrap.js', 'resources/assets/js')//Copia da pasta bootstrap/js para a pasta resources/assets/js
+			.copy(bowerDir + 'jquery/dist/jquery.js', 'resources/assets/js') //Copia da pasta jquery/js para a pasta resources/assets/js
+
+			.scripts([
+				'jquery.js',  //É important carregar primeiro o jquery para o funcionamento correto do sistema
+				'bootstrap.js' // Carrega o bootstrap.js
+			],	'public/js/scripts.js') // Diretório onde será salvo os scripts
+			
+			.less('app.less'); //Irá ler, minificar os arquivos e salvar na pasta public/assets/app.css
+	});
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<meta charset="utf-8">
 	<title>Code Agenda</title>
 </head>
